d42694466fba776759149086f66cac24958a49f8
Tricia Shi <tricia.shi@gmail.com>
2017-04-11 00:46:23 -0400

    add in layout

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 $(document).on('ready', function() {
 LEAPSCALE = 0.6;
 SWIPE_THRESH = 100;
+CELL_WIDTH = 300;
 var cursor = new Cursor();
 var fingerCursors = [new Cursor(), new Cursor(), new Cursor(), new Cursor(), new Cursor()];
 
@@ -14,12 +15,79 @@ var setUpFingerCursors = function() {
 	fingerCursors[4].setColor('purple');
 }
 
+var createTable = function(rows, cols) {
+	var t = document.createElement("table");
+	for (var r = 0; r < rows; r++) {
+		rowEle = document.createElement("tr");
+		for (var c = 0; c < cols; c++) {
+			colEle = document.createElement("td");
+			rowEle.appendChild(colEle);
+		}
+		t.appendChild(rowEle);
+	}
+	return t;
+}
+
+var findIntersectingModule = function(screenPosition) {
+	tableOrigin = $("table").offset();
+	rows = $("table tr").length;
+	cols = $("table tr td").length;
+
+	for (var r = 0; r < rows; r++) {
+		rowEle = document.createElement("tr");
+		for (var c = 0; c < cols; c++) {
+
+			if (screenPosition[0] > tableOrigin.left + CELL_WIDTH*c
+				&& screenPosition[0] < tableOrigin.left + CELL_WIDTH*(c+1)
+				&& screenPosition[1] > tableOrigin.top + CELL_WIDTH*r
+				&& screenPosition[1] < tableOrigin.top + CELL_WIDTH*(r+1)) {
+				return [r, c];
+			}
+		}
+	}
+}
+
+var makeTableFromRC = function(r, c) {
+	return "tr:nth-child(" + String(r+1) + ") td:nth-child(" + String(c+1) + ")";
+}
+var setIntersectingModule = function(r, c) {
+	$("table tr td").removeClass("active-module");
+	$("table " + makeTableFromRC(r, c)).addClass("active-module");
+}
+
+var stopIntersectingModule = function() {
+	$("table tr td").removeClass("active-module");
+}
+
+var addButtonModule = function(parentSelector, r, c) {
+	b = document.createElement("button");
+	b.innerHTML = "Press ME!";
+	b.className = "circle-btn";
+	$(parentSelector + " " + makeTableFromRC(r, c)).append(b);
+}
+
+/*// TODO: don't hardcode this
+var intersectButton = function(intersectingModule, screenPosition) {
+	if (intersectingModule[0] == 1 && intersectingModule[1] == 1) {
+		if (screenPosition[0] > tableOrigin.left + CELL_WIDTH*c
+			&& screenPosition[0] < tableOrigin.left + CELL_WIDTH*(c+1)
+			&& screenPosition[1] > tableOrigin.top + CELL_WIDTH*r
+			&& screenPosition[1] < tableOrigin.top + CELL_WIDTH*(r+1)) {
+			return [r, c];
+		}		
+	}
+}*/
+
+var addKnobModule = function(parentSelector, r, c) {
+
+}
+
 var setUpUI = function() {
 	// set up cursor
 	var c = document.createElement("canvas");
 	c.id = "cursor-canvas";
-	c.width = window.innerWidth;
-	c.height = window.innerHeight;
+	c.width = window.innerWidth*0.97;
+	c.height = window.innerHeight*0.97;
 	c.style.position = 'absolute';
 	document.body.appendChild(c);
 
@@ -32,11 +100,38 @@ var setUpUI = function() {
 		ctx.fill();
 	});
 
-	// set up modules
+	// set up layout
+	//front 
+	var d = document.createElement("div");
+	d.className = "bomb-side";
+	d.id = "bomb-front";
 	var t = document.createElement("h1");
-	t.id = "bombstate-tag";
 	t.innerHTML = "FRONT";
-	document.body.appendChild(t);
+	d.appendChild(t);
+	grid = createTable(2, 3);
+	grid.className = "module-container";
+	d.append(grid);
+
+	//back 
+	var d2 = document.createElement("div");
+	d2.className = "bomb-side";
+	d2.id = "bomb-back";
+	var t2 = document.createElement("h1");
+	t2.innerHTML = "BACK";
+	d2.appendChild(t2);	
+	grid2 = createTable(2, 3);
+	grid2.className = "module-container";
+	d2.append(grid2);
+
+	document.body.append(d);
+	document.body.append(d2);
+
+	// add modules to layout
+	addButtonModule('#bomb-front', 1, 1);
+	addKnobModule('#bomb-back', 2, 1);
+
+	// hide the back
+	$("#bomb-back").hide();
 
 	setUpFingerCursors();
 }
@@ -59,6 +154,13 @@ var vec3Dist = function(vec1, vec2) {
 Leap.loop({hand: function(hand) {
     var handPosition = hand.screenPosition();
     var cursorPosition = [handPosition[0], handPosition[1]+300];
+
+    intersectingModule = findIntersectingModule(cursorPosition);
+    if (intersectingModule) {
+    	setIntersectingModule(intersectingModule[0], intersectingModule[1]);
+    } else {
+    	stopIntersectingModule();
+    }
 
     cursor.setScreenPosition(cursorPosition);
    
@@ -85,9 +187,11 @@ Leap.loop({hand: function(hand) {
 						swiping = true;
 						bombState = 1-bombState; //TODO: abstract this away
 						if (bombState) {
-							$("#bombstate-tag").text("BACK");
+							$("#bomb-back").show();
+							$("#bomb-front").hide();
 						} else {
-							$("#bombstate-tag").text("FRONT");
+							$("#bomb-back").hide();
+							$("#bomb-front").show();
 						}
 						window.setTimeout(function() {
 							swiping = false;
@@ -98,7 +202,7 @@ Leap.loop({hand: function(hand) {
 	        }
 	    });
 
-	    updateFingerLocations(frame);
+	    //	updateFingerLocations(frame);
   }
 }).use('screenPosition', {scale: LEAPSCALE});
 
